8c62ec05490cf81d08b04dc452befcebb657ece3
samueljsong <semuall0108@gmail.com>
2023-07-04 16:40:49 -0700

    pracitce using next stripe

--- app/layout.js
+++ app/layout.js
@@ -29,7 +29,14 @@ export default function RootLayout({ children }) {
         <div className='flex-1'>
           {children}
         </div>
-        <footer>FOOTER</footer>
+        <footer className='flex items-center flex-wrap justify-center 
+        border-t border-solid border-slate-300 p-4 md:p-8'>
+          <Link href={'https://www.instagram.com/S.emual'} target='_blank'>
+            <i className="fa-brands fa-instagram text-slate-700 hover: 
+            text-slate-500 cursor-pointer text-2xl sm:text-3xl md:text-4xl">
+            </i>
+          </Link>
+        </footer>
       </body>
     </html>
   )
